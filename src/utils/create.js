1f7e914aabffc8b6d96e2ddccdaec3ab64142e36
valarliang <1780304097@qq.com>
2020-12-28 09:35:56 +0800

    create.js sync

--- src/utils/create.js
+++ src/utils/create.js
@@ -1,17 +1,28 @@
 import Vue from 'vue'
 
 export default function create(component, props) {
-  const vm = new Vue({
-    render(h) {
-      return h(component, {props})
-    }
-  }).$mount()
-  document.body.appendChild(vm.$el)
+  // 方法1：
+  // const vm = new Vue({
+  //   render(h) {
+  //     return h(component, {props})
+  //   }
+  // }).$mount()
+  // console.log(vm)
+  // document.body.appendChild(vm.$el)
 
-  const comp = vm.$children[0]
+  // const comp = vm.$children[0]
+  // comp.remove = () => {
+  //   document.body.removeChild(vm.$el)
+  //   vm.$destroy()
+  // }
+  // 方法2：
+  const VComp = Vue.extend(component)
+  const comp = new VComp({propsData: props}).$mount()
+  console.log(comp)
+  document.body.appendChild(comp.$el)
   comp.remove = () => {
-    document.body.removeChild(vm.$el)
-    vm.$destroy()
+    document.body.removeChild(comp.$el)
+    comp.$destroy()
   }
   return comp
 }
