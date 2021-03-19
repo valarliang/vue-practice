6c2fa43ee2bccba2d50ff49bfe6db7f60c6b66ec
valarliang <1780304097@qq.com>
2021-03-19 16:54:45 +0800

    注释

--- webpack.js
+++ webpack.js
@@ -4,7 +4,7 @@ const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const babel = require('@babel/core');
 
-const moduleAnalyser = (filename = './src/main.js') => {
+const moduleAnalyser = (filename = './src/main.js') => { // 得到文件名、依赖、源码
   const content = fs.readFileSync(filename, 'utf-8');
   const ast = parser.parse(content, {
     sourceType: 'module'
@@ -29,7 +29,7 @@ const moduleAnalyser = (filename = './src/main.js') => {
   }
 }
 
-const getDependenciesGraph = (entry) => {
+const getDependenciesGraph = (entry) => { // 循环得到所有依赖文件
   const entryModule = moduleAnalyser(entry);
   const graphArray = [ entryModule ];
   for(let i = 0; i < graphArray.length; i++) {
@@ -43,18 +43,18 @@ const getDependenciesGraph = (entry) => {
   return graphArray.reduce((acc, { filename, dependencies, code }) => acc[filename] = { dependencies, code }, {})
 }
 
-const generateCode = (entry) => {
+const generateCode = (entry) => { // 生成代码，递归require模块
   const graph = JSON.stringify(getDependenciesGraph(entry));
   return `
     (function(graph){
       function require(module) {
-        function localRequire(relativePath) {
+        function insideRequire(relativePath) {
           return require(graph[module].dependencies[relativePath]);
         }
         var exports = {};
         (function(require, exports, code){
           eval(code)
-        })(localRequire, exports, graph[module].code);
+        })(insideRequire, exports, graph[module].code);
         return exports;
       };
       require('${entry}')
