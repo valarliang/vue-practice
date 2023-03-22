ea08c7f297add40278d16fca9f0ed71c75bc2d57
梁琰沁 <valaryanqin@gmail.com>
2023-03-22 22:23:34 +0800

    graph demo update

--- src/views/G6graph.vue
+++ src/views/G6graph.vue
@@ -4,32 +4,6 @@
 
 <script>
 import G6 from '@antv/g6'
-const initData = {
-  // 点集
-  nodes: [
-    {
-      id: 'node1', // 节点的唯一标识
-      x: 100, // 节点横坐标
-      y: 200, // 节点纵坐标
-      label: '起始点', // 节点文本
-    },
-    {
-      id: 'node2',
-      x: 300,
-      y: 200,
-      label: '目标点',
-    },
-  ],
-  // 边集
-  edges: [
-    // 表示一条从 node1 节点连接到 node2 节点的边
-    {
-      source: 'node1', // 起始点 id
-      target: 'node2', // 目标点 id
-      label: '我是连线', // 边的文本
-    },
-  ],
-};
 export default {
   data() {
     return {
@@ -37,12 +11,53 @@ export default {
     }
   },
   mounted() {
+    // 实例化 minimap 插件
+    const minimap = new G6.Minimap({
+      size: [100, 100],
+      className: 'minimap',
+      type: 'delegate',
+    });
     this.graph = new G6.Graph({
+      plugins: [minimap],
       container: 'mountNode', // 指定挂载容器
       width: 800, // 图的宽度
       height: 500, // 图的高度
-      fitView: true,
-      fitViewPadding: [20, 40, 50, 20],
+      // fitView: true,
+      // fitViewPadding: [20, 40, 50, 20],
+      layout: {
+        // Object，可选，布局的方法及其配置项，默认为 random 布局。
+        type: 'force', // 指定为力导向布局
+        preventOverlap: true, // 防止节点重叠
+        linkDistance: 200, // 指定边距离为100
+        // nodeSize: 30 // 节点大小，用于算法中防止节点重叠时的碰撞检测。由于已经在defaultNode配置中设置了每个节点的 size 属性，则不需要在此设置 nodeSize。
+      },
+      modes: {
+        default: [
+          'drag-canvas', 'zoom-canvas', 'drag-node', // 允许拖拽画布、放缩画布、拖拽节点
+          {
+            type: 'tooltip', // 提示框
+            formatText(model) {
+              // 提示框文本内容
+              const text = 'label: ' + model.label + '<br/> class: ' + model.class;
+              return text;
+            },
+          },
+          {
+            type: 'edge-tooltip', // 边提示框
+            formatText(model) {
+              // 边提示框文本内容
+              const text =
+                'source: ' +
+                model.source +
+                '<br/> target: ' +
+                model.target +
+                '<br/> weight: ' +
+                model.weight;
+              return text;
+            },
+          },
+        ],
+      },
       // 节点在默认状态下的样式配置（style）和其他配置
       defaultNode: {
         size: 30, // 节点大小
@@ -72,6 +87,25 @@ export default {
         // 边上的标签文本配置
         labelCfg: {
           autoRotate: true, // 边上的标签文本根据边的方向旋转
+        },
+      },
+      // 节点不同状态下的样式集合
+      nodeStateStyles: {
+        // 鼠标 hover 上节点，即 hover 状态为 true 时的样式
+        hover: {
+          fill: 'lightsteelblue',
+        },
+        // 鼠标点击节点，即 click 状态为 true 时的样式
+        click: {
+          stroke: '#000',
+          lineWidth: 3,
+        },
+      },
+      // 边不同状态下的样式集合
+      edgeStateStyles: {
+        // 鼠标点击边，即 click 状态为 true 时的样式
+        click: {
+          stroke: 'steelblue',
         },
       },
     });
@@ -115,11 +149,50 @@ export default {
       });
       this.graph.data(remoteData); // 加载远程数据
       this.graph.render(); // 渲染
+      // 鼠标进入节点
+      this.graph.on('node:mouseenter', (e) => {
+        const nodeItem = e.item; // 获取鼠标进入的节点元素对象
+        this.graph.setItemState(nodeItem, 'hover', true); // 设置当前节点的 hover 状态为 true
+      });
+      // 鼠标离开节点
+      this.graph.on('node:mouseleave', (e) => {
+        const nodeItem = e.item; // 获取鼠标离开的节点元素对象
+        this.graph.setItemState(nodeItem, 'hover', false); // 设置当前节点的 hover 状态为 false
+      });
+      // 点击节点
+      this.graph.on('node:click', (e) => {
+        // 先将所有当前是 click 状态的节点置为非 click 状态
+        const clickNodes = this.graph.findAllByState('node', 'click');
+        clickNodes.forEach((cn) => {
+          this.graph.setItemState(cn, 'click', false);
+        });
+        const nodeItem = e.item; // 获取被点击的节点元素对象
+        this.graph.setItemState(nodeItem, 'click', true); // 设置当前节点的 click 状态为 true
+      });
+      // 点击边
+      this.graph.on('edge:click', (e) => {
+        // 先将所有当前是 click 状态的边置为非 click 状态
+        const clickEdges = this.graph.findAllByState('edge', 'click');
+        clickEdges.forEach((ce) => {
+          this.graph.setItemState(ce, 'click', false);
+        });
+        const edgeItem = e.item; // 获取被点击的边元素对象
+        this.graph.setItemState(edgeItem, 'click', true); // 设置当前边的 click 状态为 true
+      });
     },
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-
+<style scoped>
+/* 提示框的样式 */
+>>> .g6-tooltip {
+  border: 1px solid #e2e2e2;
+  border-radius: 4px;
+  font-size: 12px;
+  color: #545454;
+  background-color: rgba(255, 255, 255, 0.9);
+  padding: 10px 8px;
+  box-shadow: rgb(174, 174, 174) 0px 0px 10px;
+}
 </style>
