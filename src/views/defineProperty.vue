3ea14e7cbc8416c7088f830e6f45c33b0ad0d731
梁琰沁 <yanqin.liang@newtouch.com>
2023-09-07 14:28:21 +0800

    update

--- src/views/defineProperty.vue
+++ src/views/defineProperty.vue
@@ -11,7 +11,7 @@
       // console.log(obj)
       const reactive = (obj) => {
         Object.keys(obj).forEach(key => {
-          let value = obj[key]    // 利用闭包，将初始值存为value，用来（作为桥梁）实现当前key的取值和赋值操作
+          let value = obj[key]    // 利用闭包，将初始值存为value，用来（作为桥梁）同步取值和赋值的结果
           if (typeof value == 'object') reactive(value)
           Object.defineProperty(obj, key, {
             get() {
@@ -29,9 +29,9 @@
       }
       reactive(obj)
       // obj.d[0] = 'abc' // Vue为性能考虑不予实现数组响应
-      // obj.b.c = 4 // 通过外层递归挂载c的响应
+      // obj.b.c = 4 // 已有属性通过外层递归挂载c的响应
       // obj.b.e = 5 // 新增的属性无法挂载响应
-      // obj.a = {d: 3} // 通过setter内递归挂载d的响应
+      // obj.a = {d: 3} // 已有属性赋值新对象通过setter内递归挂载新对象内d的响应
       // obj.a.d = 2
     }
   }
